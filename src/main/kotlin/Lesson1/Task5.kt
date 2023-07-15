cbfa6dc1bb963e86f9cb846420abe3e2e9317ad1
Robotnik <wowerkrut3@gmail.com>
2023-07-15 21:35:42 +0300

    KS-1-5 add changes according to convention changed format Transformed seconds into hh:mm:ss format created empty package

--- src/main/kotlin/Lesson1/Task5.kt
+++ src/main/kotlin/Lesson1/Task5.kt
@@ -2,11 +2,11 @@ package Lesson1
 
 fun main(){
     val secundKosmos = 6480
-    val minutKosmos = secundKosmos/60
-    val ostatokSecundKosmos = secundKosmos%60
+    val minutKosmos = secundKosmos / 60
+    val ostatokSecundKosmos = secundKosmos % 60
 
-    val chasKosmos = minutKosmos/60
-    val ostatokChasKosmos = minutKosmos%60
+    val chasKosmos = minutKosmos / 60
+    val ostatokChasKosmos = minutKosmos % 60
 
-    print("%02d".format(chasKosmos)+":"+"%02d".format(ostatokChasKosmos)+":"+"%02d".format(ostatokSecundKosmos))
+    print(String.format("%02d:%02d:%02d",chasKosmos,ostatokChasKosmos,ostatokSecundKosmos))
 }
