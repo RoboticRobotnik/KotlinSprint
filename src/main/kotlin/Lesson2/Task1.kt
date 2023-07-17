42022f613cb4974cc316fb79e00001fa1c546623
Robotnik <wowerkrut3@gmail.com>
2023-07-17 12:18:14 +0300

    KS-2-1 Found average rate

--- src/main/kotlin/Lesson2/Task1.kt
+++ src/main/kotlin/Lesson2/Task1.kt
@@ -5,8 +5,8 @@ fun main(){
     val ocenkaStudent2 = 4
     val ocenkaStudent3 = 3
     val ocenkaStudent4 = 5
-
     val sredniiBal = (ocenkaStudent1 + ocenkaStudent2 + ocenkaStudent3 + ocenkaStudent4) / 4.toFloat()
+
 
     print(sredniiBal)
 }
