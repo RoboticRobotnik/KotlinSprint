f9036f654435bc6ba1e431837314fd3dbe33b08c
Robotnik <wowerkrut3@gmail.com>
2023-07-17 12:24:23 +0300

    KS-2-4 Found amount of additional resources

---
+++ src/main/kotlin/Lesson2/Task4.kt
@@ -0,0 +1,16 @@
+package Lesson2
+
+fun main(){
+    val baseKristallRuda = 7
+    val baseZhelezoRuda = 11
+
+    val buff = 0.2f
+
+    val buffKristallRuda = baseKristallRuda * buff
+    val buffZhelezoRuda = baseZhelezoRuda * buff
+
+    println(buffKristallRuda.toInt())
+    println(buffZhelezoRuda.toInt())
+
+
+}
