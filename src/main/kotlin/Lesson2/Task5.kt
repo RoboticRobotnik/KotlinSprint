2bf349b70639fff4b6ad0b78841030ec2348126d
Robotnik <wowerkrut3@gmail.com>
2023-07-18 11:07:30 +0300

    KS-2-5 Deleted space and renamed variable

--- src/main/kotlin/Lesson2/Task5.kt
+++ src/main/kotlin/Lesson2/Task5.kt
@@ -3,13 +3,12 @@ package Lesson2
 import kotlin.math.*
 
 fun main(){
-    val vklad = 70000.0
-    val srokVklada = 20.0
+    val vklad = 70000
+    val srokVklada = 20
     val procent = 16.7
 
+    val capitalizirovaniVklad = vklad * ((1.0 + procent / 100.0).pow(srokVklada))
 
-    val finaSumma = vklad * ((1.0 + procent / 100.0).pow(srokVklada))
-
-    println(String.format("%.3f", finaSumma))
+    println(String.format("%.3f", capitalizirovaniVklad))
 
 }
